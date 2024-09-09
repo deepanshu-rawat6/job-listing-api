d049dc79f6817c138306217a797f4cebf2f14889
Deepanshu Rawat <deepanshu.dr10@gmail.com>
2024-09-10 02:24:00 +0530

    feat: new env values

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,4 +2,12 @@ import 'dotenv/config';
 
 export default {
     PORT: process.env.PORT || 3000,
+    SALT_ROUNDS: process.env.SALT_ROUNDS,
+    JWT_SECRET: process.env.JWT_SECRET,
+    COOKIE_NAME: process.env.COOKIE_NAME,
+    NODE_ENV: process.env.NODE_ENV,
+    UI_ORIGIN: process.env.UI_ORIGIN,
+    REDIS_HOST: process.env.REDIS_HOST,
+    REDIS_PORT: process.env.REDIS_PORT,
+    REDIS_PASSWORD: process.env.REDIS_PASSWORD
 }
