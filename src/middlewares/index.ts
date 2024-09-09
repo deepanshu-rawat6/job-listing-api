117a48c09c650fe0a14ded383c1e5d95df773d73
Deepanshu Rawat <deepanshu.dr10@gmail.com>
2024-09-10 02:09:35 +0530

    feat: zod middlewares

---
+++ src/middlewares/index.ts
@@ -0,0 +1,11 @@
+import { isAuthenticated } from "./isAuthenticated";
+import { validateData } from "./validator";
+import { isUser } from "./isUser";
+import { isOwner } from "./isOwner";
+
+export {
+    isAuthenticated,
+    validateData,
+    isUser,
+    isOwner
+}
